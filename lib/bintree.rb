55a5939bd03ee9b501472591ee948310c3ce5062
Robin van de Griend <robinvdgriend@outlook.com>
2015-10-21 14:55:00 +0200

    Change build_tree to use Node.insert recursively

--- lib/bintree.rb
+++ lib/bintree.rb
@@ -2,17 +2,12 @@ require_relative 'bintree/node'
 
 module BinTree
   def self.build_tree(ary)
-    ary = ary.to_a
-    if ary.size == 0
-      nil
-    elsif ary.size == 1
-      Node.new(ary[0])
-    else
-      ary_left = ary[0..(ary.size / 2 - 1)]
-      ary_mid = ary[ary.size / 2]
-      ary_right = ary[(ary.size / 2 + 1)..-1]
+    values = ary.shuffle
+    root_node = Node.new(values.shift)
 
-      Node.new(ary_mid, left: build_tree(ary_left), right: build_tree(ary_right))
+    until values.empty?
+      root_node.insert_node(values.shift)
     end
+    root_node
   end
 end
--- lib/bintree/node.rb
+++ lib/bintree/node.rb
@@ -1,11 +1,30 @@
 module BinTree
   class Node
-    attr_accessor :value, :left, :right
+    attr_accessor :value, :left_child, :right_child
 
     def initialize(value, opts = {})
       @value = value
-      @left = opts.fetch(:left, nil)
-      @right = opts.fetch(:right, nil)
+      @left_child = opts.fetch(:left_child, nil)
+      @right_child = opts.fetch(:right_child, nil)
+    end
+
+    def insert_node(value)
+      if value <= self.value
+        if self.left_child == nil
+          self.left_child = Node.new(value)
+        else
+          self.left_child.insert_node(value)
+        end
+      elsif value > self.value
+        if self.right_child == nil
+          self.right_child = Node.new(value)
+        else
+          self.right_child.insert_node(value)
+        end
+      else
+        raise
+      end
+      self
     end
   end
 end
